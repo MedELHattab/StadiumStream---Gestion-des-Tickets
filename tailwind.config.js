5bc775b69c2e6e509668cfe6e902f7b7145c5059
MedELHattab <elhattabmohammedelarbi.com>
2023-12-28 10:12:48 +0100

    This is my first commit in my branch where I addded the the integration of my pages

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/view/*.{html,js,php}","./app/view/dashboard*.{html,js,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
